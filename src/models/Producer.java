c2a1777c7ec9efeb94a854765931b50e1c66a321
angelicacosta <20612929+angelicacosta@users.noreply.github.com>
2021-01-05 17:47:03 +0100

    Adding producer file

---
+++ src/models/Producer.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.ArrayList; 
+import java.util.Random;
+
+public class Producer extends Thread {
+	
+	ArrayList<Integer> queue;
+	int max_messages;
+	Random rand = new Random();
+	
+    public Producer(String str, ArrayList<Integer> q, int n) {
+        super(str);
+        queue = q;
+        max_messages = n;
+    }
+    
+    public void run() {
+    	int current_message; 
+        for (int i = 0; i < max_messages; i++) { 
+        	current_message = rand.nextInt(1000);
+            synchronized (this) { 
+           
+                System.out.println("Producer " + getName() + "produced" + Integer.toString(current_message)); 
+                queue.add(current_message); 
+
+            } 
+        } 
+    }
+}
