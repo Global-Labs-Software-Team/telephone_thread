76086cb491f73e26f59361c47f6c4062591be753
Daniel Rodriguez <danielrs9705@gmail.com>
2021-01-08 17:06:37 +0100

    FIx error in tests, using function start of threads

--- src/tests/TelephoneTest.java
+++ src/tests/TelephoneTest.java
@@ -7,7 +7,7 @@ public class TelephoneTest {
     public static void main(String[] args) {
         Pipeline phone1_to_phone2 = new Pipeline();
         Pipeline phone2_to_phone1 = new Pipeline();
-        new Telephone("1", phone1_to_phone2, phone2_to_phone1, 30).run(); // Replace after with start because will be a thread
-        new Telephone("2", phone2_to_phone1, phone1_to_phone2, 30).run(); // Replace after with start because will be a thread
+        new Telephone("1", phone1_to_phone2, phone2_to_phone1, 30).start(); // Replace after with start because will be a thread
+        new Telephone("2", phone2_to_phone1, phone1_to_phone2, 30).start(); // Replace after with start because will be a thread
     }
 }
